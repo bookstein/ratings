cffb25eb5346e25a73b55fd4048be9739adaff4d
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 12:46:50 -0700

    Created classes for User, Movie, Rating

--- model.py
+++ model.py
@@ -1,11 +1,36 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 
 Base = declarative_base()
 
 ### Class declarations go here
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(120), nullable=True)
+    password = Column(String(60), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+    occupation = Column(String(60), nullable=True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    title = Column(String(120))
+    release_date = Column(Date)
+    url = Column(String(120))
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, nullable = False)
+    movie_id = Column(Integer, nullable = False)
+    rating = Column(Integer, nullable = False)
 
 ### End class declarations
 
