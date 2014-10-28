d95426ce5155bba1a13df34cc7a2329394ee7eb2
Hackbright Student <info@hackbrightacademy.com>
2014-10-28 11:51:56 -0700

    Created initial signup form and handlers

--- judgment.py
+++ judgment.py
@@ -9,11 +9,18 @@ app = Flask(__name__)
 def index():
     # session = model.connect()
     print "hello"
-    user_list = session.query(model.User).limit(5).all()
+    user_list = db_session.query(model.User).limit(5).all()
     print user_list
     return render_template("user_list.html", user_list=user_list)
 
+@app.route("/signup")
+def signup_view():
+    return render_template("signup.html")
+
+@app.route("/signup", methods=["POST"])
+def signup_complete():
+    return "IT WORKED"
 
 if __name__ == "__main__":
-    session = model.connect()
+    db_session = model.connect()
     app.run(debug=True)
